91ad1fa90ba6b82be8bca5ee6d24fe421312681a
TianSirk <tiansirk@outlook.hu>
2019-09-06 22:45:32 +0200

    Create Bookadapter

---
+++ app/src/main/java/com/example/tians/booklisting/BookAdapter.java
@@ -0,0 +1,52 @@
+package com.example.tians.booklisting;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by tians on 2019. 09. 06..
+ */
+
+public class BookAdapter extends ArrayAdapter<Book> {
+
+    public BookAdapter(@NonNull Context context, int resource, @NonNull List<Book> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+        // Get the {@link Book} object located at this position in the list
+        Book currBook = (Book) getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID title
+        TextView titleTextView = (TextView) listItemView.findViewById(R.id.title);
+        // Get the titel from the current Book object and
+        // set this text on the name TextView
+        titleTextView.setText(currBook.getTitle());
+
+        // Find the TextView in the list_item.xml layout with the ID title
+        TextView authorsTextView = (TextView) listItemView.findViewById(R.id.authors);
+        // Get the authors from the current Book object and
+        // set this text on the name TextView
+        titleTextView.setText(currBook.getAuthors());
+
+        // Return the whole list item layout (containing 2 TextViews)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
